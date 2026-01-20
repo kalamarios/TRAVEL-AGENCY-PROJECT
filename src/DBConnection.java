5262ff6f81da0f687681bde49b508363a18f89de
kalamarios <marioskkts@gmail.com>
2026-01-20 15:10:00 +0200

    Initial commit

---
+++ src/DBConnection.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/travel_agency_2025?serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "07112005Marios!";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error connecting to the database", ex);
+        }
+    }
+}
